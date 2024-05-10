4eaf6ccf3c514ae1d4eb05ab55e021bd663515c5
Kyoharo <abdoataa39@gmail.com>
2024-05-10 16:19:34 +0400

    add status

--- lambda_version/main.py
+++ lambda_version/main.py
@@ -1,13 +1,6 @@
 from actions.cryptoTrader import CryptoTrader
 from time import sleep
-# def lambda_handler(event, context):
-#         trader = CryptoTrader()
-#         trader.trade()
-        
-#         return {
-#             'statusCode': 200,
-#             'body': json.dumps('Lambda function executed successfully')
-#         }
+
 
 while True:
         trader = CryptoTrader()
