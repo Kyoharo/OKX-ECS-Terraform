fa01ce6538a62402c73cfc14c3566d717b294759
Kyoharo <abdoataa39@gmail.com>
2024-05-13 14:57:54 +0400

    back to old verstion

--- actions/cryptoTrader.py
+++ actions/cryptoTrader.py
@@ -19,37 +19,6 @@ class CryptoTrader:
             return float(usdt_balance) / (self.coin_to_buy - len(available_balance) + 1)
         else:
             return float(usdt_balance)  # Default to using all USDT balance
-        
-    def check_and_write_to_file(self, key, value, file_path):
-        # Check if the key-value pair already exists in the file
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                if line.strip() == f"{key}:{value}":
-                    print("Key-value pair already exists in the file.")
-                    return
-        
-        # If the key-value pair doesn't exist, append it to the file
-        with open(file_path, 'a') as file:
-            modified_key = f"{key}-SWAP"
-            file.write(f"{modified_key}:{value}\n")
-        print("Key-value pair added to the file.")
-        
-    def delete_from_file(self, key, value, file_path):
-        # Check if the key-value pair exists in the file
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-
-        with open(file_path, 'w') as file:
-            for line in lines:
-                if line.strip() == f"{key}:{value}":
-                    print("Key-value pair deleted from the file.")
-                    return True
-                else:
-                    file.write(line)
-
-        print("Key-value pair not found in the file.")
-        return False
 
     def trade(self):
         self.buy_new_crypto()
@@ -83,7 +52,6 @@ class CryptoTrader:
                             usdt_balance_formatted = str(int(float(buy_amount)))
                             buy_crypto.post_order(new_crypto.replace("-SWAP", ""), "buy", "market", usdt_balance_formatted)
                             trader = item["uniqueName"]
-                            self.check_and_write_to_file(new_crypto, trader, "status.txt")
                             print(f"Trader: {trader}\n>> {new_crypto}: has been bought successful")
                     else:
                         print(f"{new_crypto} :Not enough USDT available")
@@ -104,22 +72,17 @@ class CryptoTrader:
                     already_exist = False
                     for balance_item in available_balance:
                         ccy = balance_item['ccy']
-                        if f"{ccy}-USDT-SWAP" == new_crypto:
+                        if f"{ccy}-USDT-SWAP" == new_crypto or f"BTC-USDT-SWAP" == new_crypto or f"ETH-USDT-SWAP" == new_crypto:
                             already_exist = True
                             break
-                    
                     if already_exist:
+                        sale_crypto = DataAction(self.api_key, self.secret_key, self.passphrase, live_trading=False)
+                        sold_crypto = new_crypto.replace("-SWAP", "")
+                        max_avail_cash = data_reader.get_max_order_cash(sold_crypto)
+                        max_avail_size = data_reader.get_max_avail_size(sold_crypto)
+                        sale_crypto.post_order(sold_crypto, "sell", "market", max_avail_size[1], max_avail_cash[1])
                         trader = item["uniqueName"]
-                        sell = self.delete_from_file(new_crypto, trader, "status.txt")
-                        if sell:
-                            sale_crypto = DataAction(self.api_key, self.secret_key, self.passphrase, live_trading=False)
-                            sold_crypto = new_crypto.replace("-SWAP", "")
-                            max_avail_cash = data_reader.get_max_order_cash(sold_crypto)
-                            max_avail_size = data_reader.get_max_avail_size(sold_crypto)
-                            sale_crypto.post_order(sold_crypto, "sell", "market", max_avail_size[1], max_avail_cash[1])
-                            print(f"Trader: {trader}\n>> {new_crypto}: has been sold successful")
-                        else:
-                            print("Anther saller")
+                        print(f"Trader: {trader}\n>> {new_crypto}: has been sold successful")
                     else:
                         trader = item["uniqueName"]
                         print(f"Trader: {trader}\n>> {new_crypto}: I don't have the crypto coin to sell")
