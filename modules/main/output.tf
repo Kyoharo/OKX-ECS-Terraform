bf5bef07fe1cb2d0bde010d3b770849c1cd9e23d
Kyoharo <abdoataa39@gmail.com>
2024-05-13 18:00:58 +0400

    Terraform

--- modules/main/output.tf
+++ modules/main/output.tf
@@ -1,3 +1,8 @@
 output "lb_dns_name" {
   value = aws_lb.LB.dns_name
 }
+
+output "repo_url" {
+  value = "${local.aws_account}.dkr.ecr.${local.aws_region}.amazonaws.com/${local.ecr_repo}:${local.image_tag}"
+  
+}
